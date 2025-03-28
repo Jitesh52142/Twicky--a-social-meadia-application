1977f15eaf844f14f5bff73e1db4dd2889835c28
jitesh bawaskar <jiteshbawaskar05@gmail.com>
2025-03-28 15:50:10 +0530

    update

--- twiky_project/wsgi.py
+++ twiky_project/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'twiky_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'twiky_projecs.settings')
 
 application = get_wsgi_application()
