e0cc473683dfe65ccbed8f4191876e38ac45e592
jitesh bawaskar <jiteshbawaskar05@gmail.com>
2025-03-28 17:28:12 +0530

    final again

--- render-build.sh
+++ render-build.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-# Run migrations before starting the server
-python manage.py makemigrations
-python manage.py migrate
+# Ensure migration files are created
+python manage.py makemigrations --noinput  
+
+# Force apply migrations
+python manage.py migrate --run-syncdb --noinput  
 
 # Collect static files
-python manage.py collectstatic --no-input
+python manage.py collectstatic --no-input  
 
 # Start the server
 gunicorn twiky_project.wsgi:application --bind 0.0.0.0:8000
